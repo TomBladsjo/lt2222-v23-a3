63ac669687668af0e269a3964dc6d0c048391e4b
Tom Södahl Bladsjö <tomsodahlbladsjo@MacBook-Air-som-tillhor-Tom.local>
2023-03-25 17:21:37 +0100

    code for part 1 of assignment

--- a3_features.py
+++ a3_features.py
@@ -3,7 +3,55 @@ import sys
 import argparse
 import numpy as np
 import pandas as pd
+from sklearn.feature_extraction.text import CountVectorizer
 # Whatever other imports you need
+
+def readfile(filename):
+    document = ''
+    main_body_start = False
+    with open(filename) as f:
+        for line in f:
+            text = line.replace('\n', ' ') 
+            if text.lstrip(' ')[:5] == '-----':
+                return document
+            if main_body_start == True:
+                document += text
+            elif text[:10] == 'X-FileName':
+                main_body_start = True
+    return document
+
+def read_data(rootdir):
+    alldocs = []
+    allauthors = []
+    for author in os.listdir(path=rootdir):
+        alldocs.extend(readfile(rootdir+author+'/'+file) for file in os.listdir(path=rootdir+author))  
+        allauthors.extend(author for file in os.listdir(path=rootdir+author))
+    return alldocs, allauthors
+
+
+def feature_table(documents, labels, dims, testsize):
+
+    vectorizer = CountVectorizer(max_features=dims)
+    X = vectorizer.fit_transform(documents).toarray()
+
+    docsdf = pd.DataFrame(X)
+    docsdf['class'] = labels
+    classes = list(docsdf['class'].unique())
+    docsdf['class_id'] = docsdf['class'].map(lambda x: classes.index(x))
+
+    grouped = docsdf.groupby("class", group_keys=False)
+    testdf = grouped.apply(lambda x: x.sample(frac=testsize/100))
+    testdf['split'] = 'test'
+    traindf = docsdf.drop(testdf.index, axis=0)
+    traindf['split'] = 'train'
+
+    fulldf = pd.concat([traindf, testdf], ignore_index=True)
+    fulldf.index.name = 'vectors'
+
+    return fulldf
+
+
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Convert directories into table.")
@@ -16,12 +64,31 @@ if __name__ == "__main__":
 
     print("Reading {}...".format(args.inputdir))
     # Do what you need to read the documents here.
+    docs, authors = read_data(args.inputdir)
+
 
     print("Constructing table with {} feature dimensions and {}% test instances...".format(args.dims, args.testsize))
     # Build the table here.
-    
+    table = feature_table(docs, authors, args.dims, args.testsize)
+
+
     print("Writing to {}...".format(args.outputfile))
     # Write the table out here.
+    table.to_csv(args.outputfile)
 
     print("Done!")
     
+
+
+
+
+
+
+
+
+
+
+
+
+
+
